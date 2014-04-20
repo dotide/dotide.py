4b336723b2d7158790ede9757b9ab8b21258b28e
Hemslo <hemslo.wang@gmail.com>
2014-04-20 21:22:06 +0800

    Add access_token operation to client
    Add access_token's CRUDL to client.

--- dotide/client.py
+++ dotide/client.py
@@ -1,3 +1,21 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class TokenAuth(requests.auth.AuthBase):
+
+    """
+    AccessToken Auth.
+    """
+
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def __call__(self, r):
+        r.headers['Authorization'] = 'Bearer ' + self.access_token
+        return r
+
+
 class Client(object):
 
     """
@@ -8,7 +26,10 @@ class Client(object):
                  client_id=None,
                  client_secret=None,
                  access_token=None,
-                 database=None):
+                 database=None,
+                 host='api.dotide.com',
+                 version='v1',
+                 secure=True):
         """
         Construct a `Client` instance.
 
@@ -26,3 +47,99 @@ class Client(object):
         self.client_secret = client_secret
         self.access_token = access_token
         self.database = database
+        self.host = host
+        self.version = version
+        self.secure = secure
+        self.session = requests.Session()
+        self.headers = {'Content-Type': 'application/json',
+                        'User-Agent': 'dotide.py',
+                        'TimeZone': 'UTC'}
+
+    def build_base_url(self):
+        """
+        Build base url
+        """
+        schema = 'https' if self.secure else 'http'
+        return '{schema}://{host}/{ver}/{db}'.format(schema=schema,
+                                                     host=self.host,
+                                                     ver=self.version,
+                                                     db=self.database)
+
+    def build_full_url(self, target):
+        """
+        Build full url.
+        """
+        return self.build_base_url() + target
+
+    def build_auth(self):
+        """
+        Build auth.
+        """
+        if self.client_id and self.client_secret:
+            auth = HTTPBasicAuth(self.client_id, self.client_secret)
+        elif self.access_token:
+            auth = TokenAuth(self.access_token)
+        else:
+            auth = None
+        return auth
+
+    def request(self, method, target, params=None, data=None):
+        """
+        An internal method that send request to server.
+        It is exposed if you need to make API calls not implemented in this
+        library or if you need to debug requests.
+
+        Parameters
+            method
+                An HTTP method (e.g. 'GET' or 'POST').
+            target
+                The target URL with leading slash (e.g. '/datastreams').
+            params
+                A dictionary of parameters to add to the request.
+            data
+                A json string. This is the body of the request.
+
+        Returns
+            parsed body
+        """
+        r = self.session.request(method,
+                                 self.build_full_url(target),
+                                 params=params,
+                                 data=data,
+                                 headers=self.headers,
+                                 auth=self.build_auth())
+
+        data = r.json() if r.content else None
+        if r.status_code >= 400:
+            raise requests.exceptions.HTTPError(data['message'] ,response=r)
+        return data
+
+    def list_access_tokens(self, params=None):
+        """
+        List access_tokens.
+        """
+        return self.request('GET', '/access_tokens', params=params)
+
+    def create_access_token(self, data=None):
+        """
+        Create an access_token.
+        """
+        return self.request('POST', '/access_tokens', data=data)
+
+    def read_access_token(self, access_token):
+        """
+        Read an access_token.
+        """
+        return self.request('GET', '/access_tokens/' + access_token)
+
+    def update_access_token(self, access_token, data=None):
+        """
+        Update an access_token.
+        """
+        return self.request('PUT', '/access_tokens/' + access_token, data=data)
+
+    def delete_access_token(self, access_token):
+        """
+        Delete an access_token.
+        """
+        return self.request('DELETE', '/access_tokens/' + access_token)
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,36 @@
 import unittest
+import mock
+import requests
+import json
 import dotide
 
+
+def mock_response(status_code=200, content=''):
+    r = requests.Response()
+    r.status_code = status_code
+    r._content = content
+    return r
 
 class TestClient(unittest.TestCase):
 
     """Tests for Client."""
+
+    def setUp(self):
+        self.client = dotide.Client(client_id='id',
+                                    client_secret='secret',
+                                    database='db')
+        self.client.session = mock.Mock()
+        self.access_token = {
+            'access_token': '61e13e47ed0b1b6f6a0ebe598d5ddba0c386a0d856487ec84e973d06b1848221',
+            'scopes': [{
+                'permissions': ['read', 'write', 'delete'],
+                'global': True,
+                'ids': [],
+                'tags': []
+            }],
+            'created_at': '2014-03-01T16:59:48.455Z',
+            'updated_at': '2014-03-01T17:01:06.690Z'
+        }
 
     def test_init_minimal(self):
         client = dotide.Client()
@@ -24,6 +50,36 @@ class TestClient(unittest.TestCase):
         self.assertEqual(client.client_secret, 'secret')
         self.assertIsNone(client.access_token)
         self.assertEqual(client.database, 'db')
+
+    def test_list_access_tokens(self):
+        self.client.session.request.return_value = mock_response(200,
+                                                                 json.dumps([self.access_token]))
+        access_tokens = self.client.list_access_tokens()
+        self.assertEqual(access_tokens, [self.access_token])
+
+    def test_create_access_token(self):
+        self.client.session.request.return_value = mock_response(201,
+                                                                 json.dumps(self.access_token))
+        access_token = self.client.create_access_token(data=json.dumps(self.access_token))
+        self.assertEqual(access_token, self.access_token)
+
+    def test_read_access_token(self):
+        self.client.session.request.return_value = mock_response(200,
+                                                                 json.dumps(self.access_token))
+        access_token = self.client.read_access_token(self.access_token['access_token'])
+        self.assertEqual(access_token, self.access_token)
+
+    def test_update_access_token(self):
+        self.client.session.request.return_value = mock_response(200,
+                                                                 json.dumps(self.access_token))
+        access_token = self.client.update_access_token(self.access_token['access_token'],
+                                                       data=json.dumps(self.access_token))
+        self.assertEqual(access_token, self.access_token)
+
+    def test_delete_access_token(self):
+        self.client.session.request.return_value = mock_response(204)
+        ret = self.client.delete_access_token(self.access_token['access_token'])
+        self.assertIsNone(ret)
 
 
 if __name__ == '__main__':
