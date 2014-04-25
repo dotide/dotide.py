85a75321647cb0619bc80ed83952eaec19f91f5b
Hemslo <hemslo.wang@gmail.com>
2014-04-25 23:38:16 +0800

    Fix test in Python 3

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,7 +8,7 @@ import dotide
 def mock_response(status_code=200, content=None):
     r = requests.Response()
     r.status_code = status_code
-    r._content = content
+    r._content = str.encode(content) if content else None
     return r
 
 
