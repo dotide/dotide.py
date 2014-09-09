bf8d995f2bfa35e7662902d312769eb857186c90
Hemslo Wang <hemslo.wang@gmail.com>
2014-09-09 21:41:08 +0800

    Update new example

--- examples/example.py
+++ examples/example.py
@@ -11,93 +11,78 @@ client = dotide.Client(DB,
                        client_id=CLIENT_ID,
                        client_secret=CLIENT_SECRET)
 
-# List datastreams
-datastreams = client.datastreams.filter(ids=['id0', 'id1'],
-                                        tags=['tag0', 'tag1'],
-                                        limit=10,
-                                        offset=10
-                                        )
-
 # Create datastream
-datastream = client.datastreams.create(id='id0',
-                                       name='name0',
-                                       type='number',
-                                       tags=['tag0'],
-                                       properties={'prop0': 1}
-                                       )
+client.datastreams.create({'id': 'id0',
+                           'name': 'name0',
+                           'tags': ['tag0'],
+                           'properties': {'prop0': 1}
+                           })
+
+# List datastreams
+client.datastreams.list({'ids': ['id0', 'id1'],
+                         'tags': ['tag0', 'tag1'],
+                         'limit': 10,
+                         'offset': 0
+                         })
 
 # Read datastream
-datastream = client.datastreams.get('id0')
+client.datastreams.get('id0')
 
 # Update datstream
-datastream.tags.append('tag1')
-datastream.save()
-
-# Update datastream directly
-datastream = client.datastreams.update('id0',
-                                       name='name0',
-                                       tags=['tag0'],
-                                       properties={'prop0': 1}
-                                       )
+client.datastreams.update('id0', {
+                          'name': 'name0',
+                          'tags': ['tag0'],
+                          'properties': {'prop0': 1}
+                          })
 
 # Delete datastream
-datastream.delete()
-
-# Delete datastream directly
 client.datastreams.delete('id0')
 
-# List datapoints
-datapoints = datastream.datapoints.filter(start=datetime(2014, 1, 1),
-                                          end=datetime.utcnow(),
-                                          order='asc',
-                                          limit=1000)
-
 # Create datapoints
-datapoint = datastream.datapoints.create(t=datetime.utcnow(), v=1)
-datapoints = datastream.datapoints.create([{'t': datetime.utcnow(), 'v': 1},
-                                           {'t': datetime.utcnow(), 'v': 2}]
-                                          )
+client.datapoints.create(datastream_id='id0',
+                         data=[[datetime.utcnow(), 1],
+                               [datetime.utcnow(), 2]])
+
+# List datapoints
+client.datapoints.filter(datastream_id='id0',
+                         params={'start': datetime(2014, 1, 1),
+                                 'end': datetime.utcnow(),
+                                 'order': 'asc',
+                                 'limit': 1000,
+                                 'offset': 0})
 
 # Read datapoint with exactly timestamp
-datapoint = datastream.datapoints.get(datetime(2014, 1, 2, 3, 4, 5, 6000))
+client.datapoints.get(datastream_id='id0',
+                      timestamp=datetime(2014, 1, 2, 3, 4, 5, 6000))
 
 # Delete datapoint
-datastream.datapoints.delete(datetime(2014, 1, 2, 3, 4, 5, 6000))
-datastream.datapoints.delete(start=datetime(2014, 1, 1),
-                             end=datetime.utcnow())
-
-# List access_token
-access_tokens = client.access_tokens.filter()
+client.datapoints.delete(datatream_id='id0',
+                         params={'start': datetime(2014, 1, 1),
+                                 'end': datetime.utcnow()})
 
 # Create access_token
-access_token = client.access_tokens.create(scopes=[{
+client.access_tokens.create({'scopes': [{
     'permissions': ['read', 'write', 'delete'],
     'global': False,
     'ids': ['id0'],
     'tags': ['tag0']
-}])
+}]})
+
+# List access_token
+access_tokens = client.access_tokens.filter({'limit': 10, 'offset': 0})
 
 # Read access_token
-access_token = client.access_tokens.get('61e13e47ed0b1b6f6a0ebe598d5ddba0c386a\
-                                        0d856487ec84e973d06b1848221')
+client.access_tokens.get(
+    '61e13e47ed0b1b6f6a0ebe598d5ddba0c386a0d856487ec84e973d06b1848221')
 
 # Update access_token
-access_token = client.access_tokens.get('61e13e47ed0b1b6f6a0ebe598d5ddba0c386a\
-                                        0d856487ec84e973d06b1848221')
-access_token.scopes = [{'permissions': ['read', 'write', 'delete'],
-                       'global': True}]
-access_token.save()
-
-# Update access_token directly
-access_token = client.access_tokens.update(
-    '61e13e47ed0b1b6f6a0ebe598d5ddba0c386a0d856487ec84e973d06b1848221',
-    scopes=[{'permissions': ['read', 'write', 'delete'], 'global': True}])
-
-# Delete access_token
-access_token = client.access_tokens.get('61e13e47ed0b1b6f6a0ebe598d5ddba0c386a\
-                                        0d856487ec84e973d06b1848221')
-access_token.delete()
+client.access_tokens.update('61e13e47ed0b1b6f6a0ebe598d5ddba0c386a0d856\
+                            487ec84e973d06b1848221',
+                            {'scopes': [
+                                {'permissions': ['read', 'write', 'delete'],
+                                 'global': True}
+                            ]})
 
 # Delete access_token directly
-client.access_tokens.delete('61e13e47ed0b1b6f6a0ebe598d5ddba0c386a0d856487ec84\
-                            e973d06b1848221')
+client.access_tokens.delete('61e13e47ed0b1b6f6a0ebe598d5ddba0c386a0d856\
+    487ec84e973d06b1848221')
